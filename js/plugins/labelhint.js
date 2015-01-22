163f1015cba705704e8d613613dae2e73bb81e2a
Josh Hannan <joshh@aycmedia.com>
2015-01-22 15:12:06 -0500

    Added Plugin Sass Files

--- js/plugins/labelhint.js
+++ js/plugins/labelhint.js
@@ -1,4 +1,49 @@
+$(function(){
+
+	// Init - File Input 
+	$('.lbl-hint').labelHint();
+
+});
+
+
 /*!
-	* Label Hint v1.3
+	* Label Hint v1.4
 */
-!function(n){n.fn.labelHint=function(){var i=n(this),s="on",e="show",t=i.find("input"),a=i.find("textarea"),l=function(){var s=n(this).closest(i).find("label");""!==this.value?s.addClass(e):s.removeClass(e)};t.add(a).bind("checkval",l),t.add(a).on("keyup",function(){n(this).trigger("checkval")}).on("focus",function(){n(this).closest(i).find("label").addClass(s)}).on("blur",function(){n(this).closest(i).find("label").removeClass(s)}).trigger("checkval")}}(jQuery);
+(function($){
+	$.fn.labelHint = function(options) {
+		var $fieldWrap = $(this),
+			onClass = "on",
+			showClass = "show",
+			theInput = $fieldWrap.find("input"),
+			theTextarea = $fieldWrap.find("textarea"),
+			theSelect = $fieldWrap.find("select");
+
+		var changeState = function(){
+			var label = $(this).closest($fieldWrap).find("label");
+			if(this.value !== ""){
+				label.addClass(showClass);
+			} else {
+				label.removeClass(showClass);
+			}
+		};
+
+		// Setup "checkval" Event (same events to trigger on input/text area, different set of events for select drop-downs).
+		theInput.add(theTextarea).bind("checkval", changeState);
+		theSelect.bind("checkval", changeState);
+
+		// Input/Textarea Triggers
+		theInput.add(theTextarea).on("keyup",function(){
+			$(this).trigger("checkval");
+		}).on("focus",function(){
+			$(this).closest($fieldWrap).find("label").addClass(onClass);
+		}).on("blur",function(){
+			$(this).closest($fieldWrap).find("label").removeClass(onClass);
+		}).trigger("checkval");
+
+		// Check State on value change
+		theInput.add(theTextarea).on("change", changeState);
+		// Select Triggers
+		theSelect.on("change", changeState).trigger("checkval");
+
+	}
+})(jQuery);
